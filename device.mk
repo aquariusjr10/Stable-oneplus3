1f738568e7a9981d35733841bc79b7885eea637a
Gaurav241 <gaurav.govinda2420@gmail.com>
2020-02-20 17:00:53 +0530

    op3: GoogleCamera by default

--- device.mk
+++ device.mk
@@ -26,6 +26,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# GoogleCamera
+$(call inherit-product-if-exists, packages/apps/GoogleCamera/oneplus3/config.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
@@ -204,7 +210,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
