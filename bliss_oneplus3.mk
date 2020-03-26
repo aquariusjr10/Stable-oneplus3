2d73e7928d17450ba0a888fea8ab0dd7cd6167f6
Deepak <deepakjeganathan@gmail.com>
2020-03-26 19:05:18 +0000

    Lets make it official huh?

--- bliss_oneplus3.mk
+++ bliss_oneplus3.mk
@@ -36,3 +36,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 
 TARGET_VENDOR := oneplus
+
+#Bliss build details
+export BLISS_BUILDTYPE=Official
+export BLISS_DEVELOPER=Deepak jr
+
